73926968ac85336d27826e2e75dfff1dd70f0881
Ben Ahlbrand <bmahlbrand@gmail.com>
2016-05-23 13:42:56 -0400

    log time format conversion

--- Utils/TimeFunc.py
+++ Utils/TimeFunc.py
@@ -7,3 +7,7 @@ def time_func_python_date_to_solr_date(dt):
 
 def time_func_solr_date_to_python_date(str):
     return datetime.strptime(str, "%Y-%m-%dT%H:%M:%SZ")
+
+
+def time_func_log_date_to_python_date(str):
+    return datetime.strptime(str, "%b %d, %Y, %I:%M %p")
