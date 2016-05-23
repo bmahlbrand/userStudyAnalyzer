4376208cea4ef2a2b7092dd9ba616551c5c44eb1
Ben Ahlbrand <bmahlbrand@gmail.com>
2016-05-23 13:43:45 -0400

    interaction keys

---
+++ Interactionr.py
@@ -0,0 +1,13 @@
+interaction_keys = ['HEATMAP', 'TOOLTIP', 'TAGMAP',
+                    'AGING', 'CONTENTLENS', 'LDA',
+                    'MSGTABLE', 'SPATIAL', 'KEYWORD_DATE',
+                    'RESET']
+
+actions = ['ENABLE', 'DISABLE', 'DEFAULT']
+
+table_actions = ['TABLE_REFRESH', 'TABLE_DOWN', 'TABLE_UP', 'TABLE_DOWNLOAD'] #is this MSGTABLE?
+
+classifier_actions = ['CLASSIFIER', 'CLASSIFIER_CREATE', 'CLASSIFIER_EDIT',
+                      'CLASSIFIER_RESET', 'CLASSIFIER_IVERB', 'CLASSIFIER_FILTER',
+                    'CLASSIFIER_UNION', 'CLASSIFIER_INTERSECT']
+
