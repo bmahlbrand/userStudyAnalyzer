6b8a366782b95ef45b2f3254a83d715c6062c014
Ben Ahlbrand <bmahlbrand@gmail.com>
2016-05-23 13:43:34 -0400

    data cleaning updates

--- Cleanr.py
+++ Cleanr.py
@@ -1,5 +1,7 @@
 from Utils.FileFunc import FileFunc as ff
+import Utils.TimeFunc as tf
 import glob
+
 
 def load(foldername, filetype):
     ret = []
@@ -10,11 +12,13 @@ def load(foldername, filetype):
         ret.append(f)
     return ret
 
+
 def load_memory_usage_data():
 
     ret = []
     ret = load('data/LOG', 'memory_usage.txt')
     return ret
+
 
 def filter_out_memory_data(filenames):
 
@@ -24,6 +28,7 @@ def filter_out_memory_data(filenames):
             ret.append(fn)
 
     return ret
+
 
 def get_usage_and_interaction(filenames):
 
@@ -39,6 +44,46 @@ def get_usage_and_interaction(filenames):
 
     return data
 
+
+def extract_students_usage(filenames):
+
+    ret = []
+
+    for fn in filenames['usage']:
+        if 'eaps' in fn:
+            ret.append(fn)
+    return ret
+
+
+def extract_students_interaction(filenames):
+
+    ret = []
+
+    for fn in filenames['interaction']:
+        if 'eaps' in fn:
+            ret.append(fn)
+    return ret
+
+
+def extract_domainexperts(filenames):
+    ret = []
+
+    for fn in filenames:
+        if 'eaps' not in fn:
+            ret.append(fn)
+    return ret
+
+
+def clean_line(line):
+    n = 5
+    groups = line.split(' ')
+    return ' '.join(groups[:n]), ' '.join(groups[n:])
+
+
+def extract_timestamp(line):
+    pass
+
+
 if __name__ == '__main__':
 
     files = load('data/LOG', '.txt')
@@ -48,5 +93,14 @@ if __name__ == '__main__':
     #     print(ff.read_file_into_list(fn))
 
     # print(load_memory_usage_data())
+    data = get_usage_and_interaction(files)
 
-    print(get_usage_and_interaction(files))
+    for fn in data['interaction']:
+        print(fn)
+    print('---')
+    for fn in data['usage']:
+        print(fn)
+
+    print(extract_students_interaction(data))
+    print(extract_timestamp('May 6, 2016, 4:07 pm CLASSIFIER ENABLE'))
+    print(tf.time_func_log_date_to_python_date('May 6, 2016, 4:07 pm'))
